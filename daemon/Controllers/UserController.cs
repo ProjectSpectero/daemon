2d495a78c8b46e22bda44fa0b97011b646b5eb70
Paul S <contact+github@winterei.se>
2017-11-24 22:26:05 +0900

    First sketch of user management.

---
+++ daemon/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Spectero.daemon.Libraries.Config;
+using Spectero.daemon.Libraries.Core.Statistics;
+using Spectero.daemon.Libraries.Services;
+
+namespace Spectero.daemon.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("v1/[controller]")]
+    [ApiExplorerSettings(IgnoreApi = false, GroupName = nameof(UserController))]
+    public class UserController : BaseController
+    {
+        public UserController(IOptionsSnapshot<AppConfig> appConfig, ILogger<UserController> logger,
+            IDbConnection db)
+            : base(appConfig, logger, db)
+        {
+
+        }
+        // GET
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
