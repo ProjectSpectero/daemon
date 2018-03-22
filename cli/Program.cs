f369f5f3b93fa28c4be1a3ffa3bfebfc8a42058b
Paul S <contact+github@winterei.se>
2018-03-23 07:48:22 +0900

    Fix broken main class definition.

--- cli/Program.cs
+++ cli/Program.cs
@@ -2,9 +2,9 @@
 
 namespace Spectero.daemon.CLI
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
