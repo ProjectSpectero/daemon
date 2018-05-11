480f7a1d60bd03560b0e76b4ba41f4031caf7cc5
Ely Haughie <elycin2@gmail.com>
2018-05-10 21:42:20 -0400

    Fixing typo with installation variable.

--- daemon/Tooling/Mac/spectero-startup.sh
+++ daemon/Tooling/Mac/spectero-startup.sh
@@ -1,6 +1,6 @@
 ﻿#!/usr/bin/env bash
 
-$SPECTERO_INSTALL_LOCATION="{install_locaction}"
+$SPECTERO_INSTALL_LOCATION="{install_location}"
 $SPECTERO_VERSION="{install_version}"
 
 if [ "$(ps -ef | grep -v grep | grep deamon.dll | wc-l)" -le 0 ]; then
