622d9e1668c39627e30817c58c06c27e6d4aa1fb
Paul S <contact+github@winterei.se>
2018-05-15 15:45:32 +0900

    Small changes to the Apm test to make it more palateable.

--- daemon-testcases/ApmUnitTest.cs
+++ daemon-testcases/ApmUnitTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using Spectero.daemon.Libraries.APM;
@@ -17,13 +18,14 @@ namespace daemon_testcases
         public void IsSerializable()
         {
             // Instantiate new APM
-            Apm localApm = new Apm();
+            var localApm = new Apm();
 
             // Get the details we need
             var details = localApm.GetAllDetails();
 
             // Here's what matters, can we serialize it?
-            JsonConvert.SerializeObject(details, Formatting.Indented);
+            // Printed out too, so we can visually inspect output.
+            Console.WriteLine(JsonConvert.SerializeObject(details, Formatting.Indented));
         }
     }
 }
