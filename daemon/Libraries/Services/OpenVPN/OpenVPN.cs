474d041f3f63dbab601e6b42422e7adec36b664f
Paul S <contact+github@winterei.se>
2018-05-19 01:00:35 +0900

    Build support infrastructure for shell integration -> LinOpenVPN.

--- daemon/Libraries/Services/OpenVPN/OpenVPN.cs
+++ daemon/Libraries/Services/OpenVPN/OpenVPN.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Net;
 using Microsoft.Extensions.Caching.Memory;
@@ -22,7 +23,12 @@ namespace Spectero.daemon.Libraries.Services.OpenVPN
         private readonly IStatistician _statistician;
         private readonly IMemoryCache _cache;
         private IEnumerable<OpenVPNConfig> _vpnConfig;
+
+
         private readonly ServiceState State = ServiceState.Halted;
+        private readonly List<string> _configsOnDisk;
+
+
 
         public OpenVPN()
         {
@@ -41,11 +47,47 @@ namespace Spectero.daemon.Libraries.Services.OpenVPN
             _statistician = statistician;
             _cache = cache;
             _localAddresses = localAddresses;
+
+            // This is tracked so we can clean it up when stopping (assuming managed stop).
+            _configsOnDisk = new List<string>();
         }
 
+        private void Initialize(IEnumerable<IServiceConfig> serviceConfigs)
+        {
+            if (serviceConfigs != null)
+                _vpnConfig = serviceConfigs as List<OpenVPNConfig>;
+
+            if (_vpnConfig == null || !_vpnConfig.Any())
+                throw new InvalidOperationException("OpenVPN init: config list was null.");
+
+            // Now, let's render the configurations into proper OpenVPN config files.
+            var renderedConfigs = _vpnConfig.Select(x => x.GetStringConfig().Result);
+
+            foreach (var config in renderedConfigs)
+            {
+                var onDiskName = Path.GetTempPath() + Guid.NewGuid() + ".ovpn";
+                using (var writer = new StreamWriter(onDiskName))
+                {
+                    writer.Write(config);
+                }
+                _configsOnDisk.Add(onDiskName);
+
+                //At this stage, we have the configs ready and on disk
+            }
+
+            // TODO: Invoke OpenVPN once per config on disk and track the process handle somewhere.
+            // TODO: We also need to hook into netlink+netfilter (or its OS specific counterparts) to enable MASQUERADE/SNAT for our NATed IPs.
+
+
+
+
+
+        }
         public void Start(IEnumerable<IServiceConfig> serviceConfig = null)
         {
-            _vpnConfig = serviceConfig as List<OpenVPNConfig>;
+           
+            Initialize(serviceConfig);
+
         }
 
         public void ReStart(IEnumerable<IServiceConfig> serviceConfig = null)
@@ -55,12 +97,17 @@ namespace Spectero.daemon.Libraries.Services.OpenVPN
 
         public void Reload(IEnumerable<IServiceConfig> serviceConfig = null)
         {
-            _vpnConfig = serviceConfig as List<OpenVPNConfig>;
+            throw new NotSupportedException();
         }
 
         public void Stop()
         {
-            throw new NotSupportedException();
+            // Let's get rid of the temporary config file(s) and clear the tracking object.
+            foreach (var fileOnDisk in _configsOnDisk)
+            {
+                File.Delete(fileOnDisk);
+            }
+            _configsOnDisk.Clear();
         }
 
         public ServiceState GetState()
