e98055be58283a0355edbab60fffba2e629c00ee
Ely Haughie <elycin2@gmail.com>
2018-06-27 23:21:40 -0400

    Revering changes, local bash error.

--- daemon/3rdParty/OpenVPN/auth.sh
+++ daemon/3rdParty/OpenVPN/auth.sh
@@ -1,1 +1,9 @@
-﻿#!/usr/bin/env bashreadarray -t lines < $1username=${lines[0]}password=${lines[1]}spectero cli auth OpenVPN $username $passwordexit $?
+﻿#!/usr/bin/env bash
+
+readarray -t lines < $1
+username=${lines[0]}
+password=${lines[1]}
+
+spectero cli auth OpenVPN $username $password
+
+exit $?
