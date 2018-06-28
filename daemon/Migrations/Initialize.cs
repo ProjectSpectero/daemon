6add4718f4fe7c2a7725a338a2190ca4eff06c12
Paul S <contact+github@winterei.se>
2018-06-28 12:01:49 +0900

    Update the CA usage definition.

--- daemon/Migrations/Initialize.cs
+++ daemon/Migrations/Initialize.cs
@@ -113,7 +113,7 @@ namespace Spectero.daemon.Migrations
                 var caPassword = PasswordUtils.GeneratePassword(48, 8);
                 var serverPassword = PasswordUtils.GeneratePassword(48, 8);
                 ca = _cryptoService.CreateCertificateAuthorityCertificate("CN=" + instanceId + ".ca.instance.spectero.io",
-                    null, null, caPassword);
+                    null, new [] { KeyPurposeID.AnyExtendedKeyUsage }, caPassword);
                 var serverCertificate = _cryptoService.IssueCertificate("CN=" + instanceId + ".instance.spectero.io",
                     ca, null, new[] { KeyPurposeID.IdKPServerAuth }, serverPassword);
 
