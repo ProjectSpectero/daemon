fb08404a8c039486ad8dc5bec2ee60c9170f2e56
Ely Haughie <elycin2@gmail.com>
2018-06-27 23:34:26 -0400

    Fixing auth.sh to support debugging

--- daemon/3rdParty/OpenVPN/auth.sh
+++ daemon/3rdParty/OpenVPN/auth.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+# Read the provided input.
 readarray -t lines < $1;
+
+# Sort values into variables.
 username=${lines[0]};
 password=${lines[1]};
 
-spectero cli auth OpenVPN $username $password;
+# Developer sanity check.
+if [ ! -f ../../../cli/bin/Debug/netcoreapp2.1/Spectero.daemon.CLI.dll ]; then
+    # The daemon is likely running this, run it as expected.
+    spectero auth OpenVPN $username $password;
+else
+    # The user is likely a developer, run the relative build.
+    dotnet ../../../cli/bin/Debug/netcoreapp2.1/Spectero.daemon.CLI.dll auth OpenVPN $username $password;
+fi
 
 exit $?;
