51f430ca785f40facf43a7c0aaf656a9af8fff32
Ely Haughie <elycin2@gmail.com>
2018-07-06 21:37:35 -0400

    Fixing SNAT rule to support --to interface-address

--- daemon/Libraries/Core/Firewall/Rule/NetworkRuleTemplates.cs
+++ daemon/Libraries/Core/Firewall/Rule/NetworkRuleTemplates.cs
@@ -5,7 +5,7 @@
     /// </summary>
     public class NetworkRuleTemplates
     {
-        public const string SNAT = "-t nat POSTROUTING -p TCP -o {interface-name} -J SNAT --to {network}";
+        public const string SNAT = "-t nat POSTROUTING -p TCP -o {interface-name} -J SNAT --to {interface-address}";
         public const string MASQUERADE = "POSTROUTING -S {network} -o {interface-name} -J MASQUERADE";
     }
 }
