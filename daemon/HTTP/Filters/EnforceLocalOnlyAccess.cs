0357ec5173db3511be170f6f21e08cb58e76d28b
Paul S <contact+github@winterei.se>
2018-08-06 15:59:18 +0900

    OP-11: add header preamble for the license.

--- daemon/HTTP/Filters/EnforceLocalOnlyAccess.cs
+++ daemon/HTTP/Filters/EnforceLocalOnlyAccess.cs
@@ -1,4 +1,20 @@
-﻿using System.Net;
+﻿/*
+    Spectero Daemon - Daemon Component to the Spectero Solution
+    Copyright (C)  2017 Spectero, Inc.
+
+    Spectero Daemon is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Spectero Daemon is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://github.com/ProjectSpectero/daemon/blob/master/LICENSE>.
+*/
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
