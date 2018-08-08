c81df2715f74d7b27d6062c0106a3ee77aac922f
Ely Haughie <elycin2@gmail.com>
2018-08-08 00:01:10 -0400

    Committing partial changes to update job for update internals.

--- daemon/Jobs/UpdaterJob.cs
+++ daemon/Jobs/UpdaterJob.cs
@@ -1,7 +1,25 @@
-﻿using System;
+﻿/*
+    Spectero Daemon - Daemon Component to the Spectero Solution
+    Copyright (C)  2017 Spectero, Inc.
+
+    Spectero Daemon is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Spectero Daemon is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://github.com/ProjectSpectero/daemon/blob/master/LICENSE>.
+*/
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -70,9 +88,39 @@ namespace Spectero.daemon.Jobs
             var releaseInformation = GetReleaseInformation();
 
             // Compare
-            if (releaseInformation.channels[_config.Updater.ReleaseChannel.ToString()] != AppConfig.version)
+            if (releaseInformation.channels.GetValueOrDefault(_config.Updater.ReleaseChannel.ToString(), AppConfig.version) != AppConfig.version)
             {
                 // Update available.
+                var newVersion = releaseInformation.channels[_config.Updater.ReleaseChannel.ToString()];
+                var targetDirectory = Path.Combine(RootInstallationDirectory, newVersion);
+                var targetArchive = Path.Combine(RootInstallationDirectory, "${newVersion}.zip");
+
+                // Log to the console that the update is available.
+                _logger.LogInformation("There is a update available for the spectero daemon: " + newVersion);
+
+                // Get the download link
+                var downloadLink = releaseInformation.versions[newVersion].download;
+
+                // Download
+                using (WebClient webClient = new WebClient())
+                    webClient.DownloadFile(new Uri(downloadLink), targetArchive);
+
+                // Extract
+                ZipFile.ExtractToDirectory(targetArchive, targetDirectory);
+
+                // Copy the databases
+                foreach (string databasePath in GetDatabasePaths())
+                {
+                    var basename = new FileInfo(databasePath).Name;
+                    var databaseDestPath = Path.Combine(targetDirectory, "daemon", "Database", basename);
+                    File.Copy(databasePath, databaseDestPath);
+                }
+
+                // Fix the symlinks.
+                // TODO: Invoke System - May need to be platform specific.
+
+                // Restart the service.
+                // TODO: Overview the instllation scripts and make sure they use the latest directory
             }
         }
 
@@ -91,5 +139,13 @@ namespace Spectero.daemon.Jobs
                 throw new InternalError(msg);
             }
         }
+
+        private string RootInstallationDirectory => Directory.GetParent(
+            Directory.GetParent(Program.GetAssemblyLocation()).FullName
+        ).FullName;
+
+        private string DatabaseDirectory => Path.Combine(Program.GetAssemblyLocation(), _config.DatabaseDir);
+
+        private string[] GetDatabasePaths() => Directory.GetFiles(DatabaseDirectory, "*.sqlite", SearchOption.TopDirectoryOnly);
     }
 }
