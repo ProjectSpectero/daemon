fbb223a47a74298b0b57a7db2009efaf02ebb737
Ely Haughie <elycin2@gmail.com>
2018-08-17 20:03:31 -0400

    Writing short test case for WinAPI Symlink

---
+++ daemon-testcases/SymlinkTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using Spectero.daemon.Libraries.Symlink;
+
+namespace daemon_testcases
+{
+    [TestFixture]
+    public class SymlinkTest
+    {
+        [Test]
+        // Example function to test if the Windows API for Symlink Creation is working.
+        public void WindowsSymlinkExample()
+        {
+            var symlinkLib = new Symlink();
+            if (symlinkLib.Environment.Create("C:/Users/Public/Testing", "C:/Users/Public/"))
+            {
+                // Symlink creation was successful - delete it and pass.
+                symlinkLib.Environment.Delete("C:/Users/Public/Testing");
+                Assert.Pass();
+            }
+            else
+            {
+                // Symlink failed to be created
+                Assert.Fail();
+            }
+        }
+    }
+}
