f0475877cb035a5541212c1040be86b9bf1ec8b3
Ely Haughie <elycin2@gmail.com>
2018-08-18 04:46:47 -0400

    Implementing windows symlink support.

--- daemon-testcases/SymlinkTest.cs
+++ daemon-testcases/SymlinkTest.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
 using NUnit.Framework;
 using Spectero.daemon.Libraries.Config;
+using Spectero.daemon.Libraries.Core.ProcessRunner;
+using Spectero.daemon.Libraries.Services;
 using Spectero.daemon.Libraries.Symlink;
 
 namespace daemon_testcases
@@ -8,17 +14,36 @@ namespace daemon_testcases
     [TestFixture]
     public class SymlinkTest
     {
+        private Mock<IService> mockedService;
+        private Mock<ILogger<ProcessRunner>> mockedLogger;
+        private Mock<IOptionsMonitor<AppConfig>> mockedMonitoredConfig;
+        private readonly ProcessRunner _runner;
+
+        public SymlinkTest()
+        {
+            mockedService = new Mock<IService>();
+            mockedService.Setup(x => x.GetState()).Returns(ServiceState.Running);
+            mockedLogger = new Mock<ILogger<ProcessRunner>>();
+            mockedMonitoredConfig = new Mock<IOptionsMonitor<AppConfig>>();
+            mockedMonitoredConfig.Setup(x => x.CurrentValue).Returns(new AppConfig());
+            _runner = new ProcessRunner(mockedMonitoredConfig.Object, mockedLogger.Object);
+        }
+
         [Test]
         // Example function to test if the Windows API for Symlink Creation is working.
         public void WindowsSymlinkExample()
         {
             if (AppConfig.isWindows)
             {
+                var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                var appDataSymlink = Path.Combine(appData, "symlink");
+
                 var symlinkLib = new Symlink();
-                if (symlinkLib.Environment.Create("C:/Users/Public/Testing", "C:/Users/Public/"))
+                symlinkLib.processRunner = _runner;
+                if (symlinkLib.Environment.Create(appDataSymlink, appData))
                 {
                     // Symlink creation was successful - delete it and pass.
-                    symlinkLib.Environment.Delete("C:/Users/Public/Testing");
+                    symlinkLib.Environment.Delete(appDataSymlink);
                     Assert.Pass();
                 }
                 else
@@ -27,6 +52,7 @@ namespace daemon_testcases
                     Assert.Fail();
                 }
             }
+
             Assert.Pass("Linux Detected - Asserting as true for this test case.");
         }
     }
